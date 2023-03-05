8be45fa05ec25dffdc114cb255f93e90eebf2b72
Pratik-Jodgudri <pratikjodpj@gmail.com>
2023-03-05 15:27:57 +0530

    modified:   RabinKarp.cpp

--- RabinKarp.cpp
+++ RabinKarp.cpp
@@ -5,58 +5,58 @@ using namespace std;
 
 int main()
 {
-    //string p, s;
-    string s="ABCCDDAEFGCDDF"; //    "ABAAABCDBBABCDDEBCABC";
-    string p=  "CDD"; //ABC
-    
-    int plen = p.length();
+    string s, p;
+    s = "ABCCDDAEFGCDDF";
+    p = "CDD";
     int slen = s.length();
+    int plen = p.length();
 
-    int ph = 0, sh = 0, h = 1, maxchar = 10;
+    int sh = 0, ph = 0, h = 1, maxchar = 10;
 
-    for (int i = 0; i < plen; i++) // cal pow(maxchar, plen-1)%prime
+    for (int i = 0; i < plen-1; i++)
     {
         h = (h * maxchar) % prime;
     }
 
-    for (int i = 0; i < plen; i++) // cal p-hash and s-hash
+    for (int i = 0; i < plen; i++)
     {
         ph = (ph * maxchar + p[i] - 65 + 1) % prime;
         sh = (sh * maxchar + s[i] - 65 + 1) % prime;
     }
 
-    cout << "hash value of the pattern " << p << " is " << ph << endl;
-        cout << "hash value of the window " << s.substr(0, plen) << " is " << sh << endl;
+    cout << "Hash value of pattern " << p << " -> " << ph << endl;
+    cout << "Hash value of window " << s.substr(0, plen) << " -> " << sh << endl;
 
-    for (int i = 0; i < slen - plen; i++)
+    for (int i = 0; i <= slen - plen; i++)
     {
         if (ph == sh)
         {
-            int flag = 0, count = 0;
-
+            int count = 0;
             for (int j = 0; j < plen; j++)
             {
-                if (s[i + j] == p[j])
+                if (p[j] == s[i + j])
+                {
                     count++;
+                }
                 else
                     break;
             }
             if (count == plen)
             {
-                cout << "Position: " << i;
-                flag = 1;
+                cout << "Pattern matched at: " << i << endl;
             }
         }
 
         if (i < slen - plen)
         {
-            sh = (((sh - (s[i] - 65 + 1)) * h) * maxchar + ((s[i + plen] - 65 + 1) * h)) % prime;
+            sh = ((sh - (s[i] - 65 + 1) * h) * maxchar + (s[i + plen] - 65 + 1)) % prime;
 
             while (sh < 0)
                 sh += prime;
 
-            cout << "hash value of the window " << s.substr(i + 1, plen) << " is " << sh << endl;
+            cout << "Hash value of window " << s.substr(i + 1, plen) << " -> " << sh << endl;
         }
+        
     }
 
     return 0;

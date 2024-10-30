d25c93bee11fa1177a18094d43732ad14c0d6cd8
Pratik Jodgudri <pratikjodpj@gmail.com>
2024-10-30 22:39:08 +0530

    modified:   CodeForces/1300B.cpp

--- CodeForces/1300B.cpp
+++ CodeForces/1300B.cpp
@@ -3,7 +3,26 @@ using namespace std;
 
 int main()
 {
-    
+    int t;
+    cin >> t;
+
+    while (t)
+    {
+        int n;
+        cin >> n;
+
+        vector <int> v(2*n + 1);
+
+        for (int i = 1; i <= 2*n; i++){
+            cin >> v[i];
+        }
+
+        sort(v.begin(), v.end());
+
+        cout << v[n + 1] - v[n] << endl;
+
+        t--;
+    }
 
     return 0;
 }
